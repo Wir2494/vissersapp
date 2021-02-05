080d2065d4910f8d7f307f35ff623c7b5898983e
wiraash <wirmatabadal@gmail.com>
2021-02-04 22:22:39 -0300

    Initial Commit and also enabled PWA functionality

---
+++ serviceworker.js
@@ -0,0 +1,14 @@
+// Service worker install
+self.addEventListener('install', evt => {
+    console.log('service worker installed.');
+});
+
+// Activation Listen Service Worker
+self.addEventListener('activate', evt => {
+    console.log('service worker activated.');
+});
+
+// Add fetch event
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt);
+})

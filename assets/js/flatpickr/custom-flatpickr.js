ba103d8c63062f7c8e35fea09e8d2d8f5f3f823b
wiraash <wirmatabadal@gmail.com>
2021-02-18 22:06:42 -0300

    Created the profile page for a boat. Also added a more options button on the profile page. Further more added a add-trip page

--- assets/js/flatpickr/custom-flatpickr.js
+++ assets/js/flatpickr/custom-flatpickr.js
@@ -1,13 +1,11 @@
 // Flatpickr
 
 var f1 = flatpickr(document.getElementById('basicFlatpickr'));
-var f2 = flatpickr(document.getElementById('dateTimeFlatpickr'), {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i",
-});
+var f2 = flatpickr(document.getElementById('basicFlatpickr2'));
 var f3 = flatpickr(document.getElementById('rangeCalendarFlatpickr'), {
     mode: "range",
 });
+console.log(f3)
 var f4 = flatpickr(document.getElementById('timeFlatpickr'), {
     enableTime: true,
     noCalendar: true,
